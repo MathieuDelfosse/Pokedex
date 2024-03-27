67e784eabcafccd37aab1fa48a61b519fef11319
Mathieu DELFOSSE <mat.delfosse@gmail.com>
2024-03-27 16:33:50 +0100

    Props added

--- src/Components/PokemonCard.jsx
+++ src/Components/PokemonCard.jsx
@@ -1,22 +1,9 @@
-function PokemonCard () {
-    
-    const pokemon = (pokemonList[0]);
+function PokemonCard({pokemon}) {
 
-    return <figure>
-        {pokemon.imgSrc? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
-        <figcaption>{pokemon.name}</figcaption>
-    </figure>
+return <figure>
+          {pokemon.imgSrc ? (<img src={pokemon.imgSrc} alt={pokemon.name}/>) : (<p>???</p>)}
+          <figcaption>{pokemon.name}</figcaption>
+      </figure>
 }
-
-const pokemonList = [
-    {
-      name: "Bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "Mew",
-    },
-  ];
 
 export default PokemonCard
