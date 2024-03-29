5459bac12750eaea0aa4d8db9d2b8fe92eee0c3f
Mathieu DELFOSSE <mat.delfosse@gmail.com>
2024-03-29 16:08:22 +0100

    added states

--- src/App.jsx
+++ src/App.jsx
@@ -5,24 +5,56 @@ import './App.css'
 import MyTitle from './Components/MyTitle'
 import PokemonCard from './Components/PokemonCard'
 
+
+
 function App() {
+
+  const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  console.log('mon pokemon index est :', pokemonIndex)
+
+  const handleClickNext = () => {
+    if (pokemonIndex < pokemonList.length - 1) { setPokemonIndex(pokemonIndex + 1) }
+    else { return }
+  }
+  const handleClickPrevious = () => {
+    if (pokemonIndex === 0) { return }
+    else { setPokemonIndex(pokemonIndex - 1) }
+  }
 
   return (
     <div>
-      <PokemonCard pokemon={pokemonList[0]} />
+      <PokemonCard pokemon={pokemonList[pokemonIndex]} />
+      <button disabled={pokemonIndex > 0 ? false : true} onClick={handleClickPrevious}>Précédent</button>
+      <button disabled={pokemonIndex < pokemonList.length-1 ? false : true} onClick={handleClickNext}>Suivant</button>
     </div>
   )
-}
-
-const pokemonList = [
-  {
-    name: "Bulbasaur",
-    imgSrc:
-      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "Mew",
-  },
-];
+};
 
 export default App
